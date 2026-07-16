c3dbd8fe4157f1800366c28abd1f2cc7a59ae17a
Natan Oliveira <natanvsnatan@gmail.com>
2026-07-16 16:33:26 -0300

    7.1.2

--- script.js
+++ script.js
@@ -6,16 +6,23 @@ function tema(){
     if (corFundo === "rgb(0, 0, 0)"){
         document.body.style.backgroundColor = "rgb(255, 255, 255)";
         document.querySelector(".h1natan").style.color = "rgb(0,0,0)";
-        document.querySelector(".navbar").style.backgroundColor = "rgba(0, 0, 0, 0.8)";
-        
+        document.querySelector(".navbar").style.backgroundColor = "rgb(51, 51, 51)";
+        document.querySelector(".divBtnTema").style.flexDirection = "row-reverse"; 
+        document.querySelector(".divBtnTema").style.backgroundColor = "rgb(89, 89, 89)"
+        document.querySelector(".bt2").style.backgroundColor = "rgb(51, 51, 51)";
+        document.querySelector(".spanBtn").innerHTML = '⏾'
+        document.querySelector(".spanBtn").style.color = "rgb(255, 255, 255)"
         splineEscura.style.display = "none";
         splineClara.style.display = "block";
     }
     else{
         document.body.style.backgroundColor = "rgb(0, 0, 0)";
         document.querySelector(".h1natan").style.color = "rgb(255,255,255)";
-        document.querySelector(".navbar").style.backgroundColor = "rgba(255, 255, 255, 0.8)";
-        
+        document.querySelector(".navbar").style.backgroundColor = "rgb(51, 51, 51)";
+        document.querySelector(".divBtnTema").style.backgroundColor = "rgb(89, 89, 89)"
+        document.querySelector(".divBtnTema").style.flexDirection = "row"; 
+        document.querySelector(".spanBtn").innerHTML = '☼︎'
+        document.querySelector(".spanBtn").style.color = "rgb(255,255,255)"
         splineClara.style.display = "none";
         splineEscura.style.display = "block";
     }
