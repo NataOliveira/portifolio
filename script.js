484c850db3bcf108d87fc9a67eefe57375cc1bd2
Natan Oliveira <natanvsnatan@gmail.com>
2026-07-17 15:13:40 -0300

    7.2

--- script.js
+++ script.js
@@ -4,26 +4,22 @@ function tema(){
     const splineClara = document.querySelector(".spline-clara");
 
     if (corFundo === "rgb(0, 0, 0)"){
-        document.body.style.backgroundColor = "rgb(255, 255, 255)";
-        document.querySelector(".h1natan").style.color = "rgb(0,0,0)";
-        document.querySelector(".navbar").style.backgroundColor = "rgb(51, 51, 51)";
+
         document.querySelector(".divBtnTema").style.flexDirection = "row-reverse"; 
-        document.querySelector(".divBtnTema").style.backgroundColor = "rgb(89, 89, 89)"
-        document.querySelector(".bt2").style.backgroundColor = "rgb(51, 51, 51)";
-        document.querySelector(".spanBtn").innerHTML = '⏾'
-        document.querySelector(".spanBtn").style.color = "rgb(255, 255, 255)"
+        document.querySelector(".spanBtn").innerHTML = '☼︎'
         splineEscura.style.display = "none";
         splineClara.style.display = "block";
+        document.documentElement.setAttribute('tema', 'light');
     }
     else{
-        document.body.style.backgroundColor = "rgb(0, 0, 0)";
-        document.querySelector(".h1natan").style.color = "rgb(255,255,255)";
-        document.querySelector(".navbar").style.backgroundColor = "rgb(51, 51, 51)";
-        document.querySelector(".divBtnTema").style.backgroundColor = "rgb(89, 89, 89)"
+        document.documentElement.removeAttribute('tema');
         document.querySelector(".divBtnTema").style.flexDirection = "row"; 
-        document.querySelector(".spanBtn").innerHTML = '☼︎'
-        document.querySelector(".spanBtn").style.color = "rgb(255,255,255)"
+        document.querySelector(".spanBtn").innerHTML = '⏾'
         splineClara.style.display = "none";
         splineEscura.style.display = "block";
     }
 }
+
+
+
+   
